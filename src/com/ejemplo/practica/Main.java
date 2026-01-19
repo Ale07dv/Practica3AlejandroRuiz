aeec7cd7162b0ebec1be9c56144092f122370579
Alejandro Ruiz Martínez <alejandro.ruizmartinez07.dv@gmail.com>
2026-01-19 14:15:38 +0100

    Proyecto Java Inicial

---
+++ src/com/ejemplo/practica/Main.java
@@ -0,0 +1,13 @@
+package com.ejemplo.practica;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	        System.out.println("Tu Nombre y apellidos");
+	        System.out.println("Proyecto Git inicial funcionando correctamente");
+
+	}
+
+}
